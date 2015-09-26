128b08374d329fc213e8678ed7873ee031905a0c
Root <parallax_horizon@zoho.com>
2015-09-26 03:13:59 -0400

    Initial Yo generated

---
+++ typings/tsd.d.ts
@@ -0,0 +1,9 @@
+
+/// <reference path="node/node.d.ts" />
+/// <reference path="react/react.d.ts" />
+/// <reference path="github-electron/electron-prebuilt.d.ts" />
+/// <reference path="github-electron/github-electron-main.d.ts" />
+/// <reference path="github-electron/github-electron-renderer.d.ts" />
+/// <reference path="github-electron/github-electron.d.ts" />
+/// <reference path="lunr/lunr.d.ts" />
+/// <reference path="dragula/dragula.d.ts" />
